cf4c36c0d080c3b806b8945f3141aeab800c56ed
kawan024 <kawan.rodrigues.santos@escola.pr.gov.br>
2025-06-12 12:12:38 +0000

    envio

---
+++ exercio13.py
@@ -0,0 +1,19 @@
+def e_bissexto(ano):
+  """Verifica se um ano é bissexto."""
+
+  if ano % 4 == 0:
+    if ano % 100 == 0:
+      if ano % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
+
+ano = int(input("Digite um ano: "))
+if e_bissexto(ano):
+  print(f"{ano} é um ano bissexto.")
+else:
+  print(f"{ano} não é um ano bissexto.")
