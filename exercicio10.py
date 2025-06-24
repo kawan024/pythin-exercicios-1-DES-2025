84b1fe48a85fcee460471e9674803ff030ad1305
kawan024 <kawan.rodrigues.santos@escola.pr.gov.br>
2025-06-24 13:34:44 +0000

    exercicio10 pronto

--- exercicio10.py
+++ exercicio10.py
@@ -5,4 +5,4 @@ if salario_mensal > 3000:
     print(f"A parcela máxima que ela pode ter é: R$ {parcela_maxima:.2f}")
 else:
     print("O salário do mês de Renata é inferior a R$ 3.000,00.")
-    print("Ela não atende ao critério de salário mínimo.") 
+    print("Ela não atende ao critério de salário mínimo.")
